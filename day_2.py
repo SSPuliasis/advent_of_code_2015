774301ba2fa52abc9b57fef69fdf3b13f2ac47ba
SSPuliasis <56365576+SSPuliasis@users.noreply.github.com>
2022-02-14 13:21:23 +0000

    Update day_2.py

--- day_2.py
+++ day_2.py
@@ -1,4 +1,3 @@
-#Part 1
 class box:
     def __init__ (self, l, w, h):
         self.l = l
@@ -14,7 +13,16 @@ class box:
         self.paper_amount = 2*(self.l*self.w + self.l*self.h + self.w*self.h) + self.smallest_side
         #print('wrapping paper needed: ', self.paper_amount)
         
+    def ribbon(self):
+        lh = (self.l + self.h)*2
+        lw = (self.l + self.w)*2
+        hw = (self.h + self.w)*2
+        self.smallest_perimeter = min(lh, lw, hw)
+        self.volume = self.l * self.w * self.h
+        self.ribbon = self.smallest_perimeter + self.volume
+        
 total_amount_of_wrapping_paper = 0
+total_amount_of_ribbon = 0
 with open('day2_input.txt') as inputfile:
     for line in inputfile:
         l = int(line.split('x')[0])
@@ -24,5 +32,7 @@ with open('day2_input.txt') as inputfile:
         h = int(h)
         single_box = box(l, w, h)
         single_box.wrapping_paper()
+        single_box.ribbon()
         total_amount_of_wrapping_paper += single_box.paper_amount
-print('wrapping paper: ', total_amount_of_wrapping_paper)
+        total_amount_of_ribbon += single_box.ribbon
+print('wrapping paper: ', total_amount_of_wrapping_paper, 'ribbon: ', total_amount_of_ribbon)
